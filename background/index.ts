1a2020857402769ca96aa9be1d798f8e00906988
fzk <458813868@qq.com>
2024-08-08 09:28:13 +0800

    检查更新插件

--- background/index.ts
+++ background/index.ts
@@ -1,9 +1,22 @@
-export {}
-
 import "@plasmohq/messaging/background"
 
-chrome.runtime.onInstalled.addListener(function(object) {
+export {}
+
+// 初始化设置
+chrome.runtime.onInstalled.addListener(function (object) {
   if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.tabs.create({ url: chrome.runtime.getURL("options.html") })
+  }
+})
+
+// 检查更新
+chrome.runtime.requestUpdateCheck(function (status, details) {
+  if (status === "update_available") {
+    console.log("有更新可用！版本" + details.version)
+    chrome.runtime.reload()
+  } else if (status === "no_update") {
+    console.log("没有可用更新。")
+  } else if (status === "throttled") {
+    console.log("更新检查被限流。")
   }
 })
