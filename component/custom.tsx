8e9362ad8e5fc87ce37f8c371ba7456b8a94351c
fzk <458813868@qq.com>
2024-09-05 11:18:24 +0800

    增加代码快速下载

--- component/custom.tsx
+++ component/custom.tsx
@@ -1,3 +1,7 @@
+import { DownloadOutlined } from "@ant-design/icons"
+import { useEffect, useState } from "react"
+
+import { sendToContentScript } from "@plasmohq/messaging"
 import { useStorage } from "@plasmohq/storage/hook"
 
 import { i18n } from "~tools"
@@ -8,9 +12,39 @@ export default function Custom() {
   )
   const [cssCode, setCssCode] = useStorage("custom-cssCode")
 
+  const [codes, setCodes] = useState([])
+
+  useEffect(() => {
+    getCodes()
+  }, [])
+
+  async function getCodes() {
+    const res = await sendToContentScript({ name: `getCodes` })
+    res?.codes && setCodes(res?.codes)
+  }
+
+  function downloadCode(index) {
+    sendToContentScript({ name: `downloadCode`, body: { index } })
+  }
+
+  function pinCode(index) {
+    sendToContentScript({ name: `scrollIntoViewCode`, body: { index } })
+  }
+
   return (
     <fieldset>
       <legend>{i18n("customConfig")}</legend>
+      {codes.map((code, index) => (
+        <div className="item code" onClick={() => pinCode(index)} key={index}>
+          <span className="codeTxt">
+            {index + 1}-{JSON.stringify(code)}
+          </span>
+          <DownloadOutlined
+            style={{ color: "#52c41a", fontSize: "16px" }}
+            onClick={() => downloadCode(index)}
+          />
+        </div>
+      ))}
       <div className="item">
         <span>{i18n("customCssCode")}</span>
         <input
--- contents/custom.tsx
+++ contents/custom.tsx
@@ -1,16 +1,20 @@
-import { useEffect } from "react"
+import { Readability } from "@mozilla/readability"
+import { useEffect, useState } from "react"
 
 import { useMessage } from "@plasmohq/messaging/hook"
 import { useStorage } from "@plasmohq/storage/hook"
 
-import { addCss, setIcon } from "~tools"
+import { addCss, saveHtml, setIcon } from "~tools"
 
+const documentClone = document.cloneNode(true)
+const article = new Readability(documentClone as Document, {}).parse()
 setIcon(false)
 
 export default function Custom() {
   const [runCss] = useStorage<boolean>("custom-runCss")
   const [cssCode] = useStorage<string>("custom-cssCode")
   const [closeLog] = useStorage("config-closeLog", true)
+  const [codes, setCodes] = useState([])
 
   useEffect(() => {
     runCss && runCssFunc()
@@ -20,12 +24,46 @@ export default function Custom() {
     if (req.name == "custom-isShow") {
       res.send({ isShow: true })
     }
+    if (req.name == "getCodes") {
+      res.send({ codes: getCodes() })
+    }
+    if (req.name == "scrollIntoViewCode") {
+      scrollIntoViewCode(req.body)
+    }
+    if (req.name == "downloadCode") {
+      downloadCode(req.body)
+    }
   })
 
   /* 插入自定义css代码 */
   function runCssFunc() {
     closeLog || console.log("插入自定义css代码", cssCode)
     addCss(cssCode)
+  }
+
+  function getCodes() {
+    let codes = []
+    let codesTxt = []
+    const _codes = document.querySelectorAll("code")
+    _codes.forEach((code) => {
+      const pre = code.closest("pre")
+      if (pre) {
+        codes.push(code)
+        codesTxt.push(code.innerText.replace(/\n/g, ""))
+      }
+    })
+    setCodes(codes)
+    return codesTxt
+  }
+
+  function scrollIntoViewCode(data) {
+    const code = codes[data.index]
+    code.scrollIntoView()
+  }
+
+  function downloadCode(data) {
+    const code = codes[data.index]
+    saveHtml(code, article.title)
   }
 
   return <div style={{ display: "none" }}></div>
